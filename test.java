1c32eb0cf9038d695e7aca21dd509c63a79c6f3f
LeEmperor <bohdanpurtell@gmail.com>
2022-03-25 16:02:56 -0400

    initial

---
+++ test.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class test {
+    public static void main(String[] args) throws Exception {
+        Scanner Input = new Scanner(System.in);
+        String line = Input.nextLine();
+        System.out.println(line);
+        Input.close();
+    }
+
+}
