eb8dba2a6a4a7348ed6a604b6317b23d8691510f
LeEmperor <bohdanpurtell@gmail.com>
2022-03-31 17:40:06 -0400

    this is a generic message

--- day8.py
+++ day8.py
@@ -2,15 +2,15 @@ import pprint
 
 if __name__ == "__main__":
     data = [];
-    cleanInstructions = [];
-    global accum;
+    index = 0;
     accum = 0;
-    lineNum = 0;
 
     with open("day8.txt", "r") as f:
-        for instruction in f:
-            data.append(instruction.strip());
-   
-   for line in data:
-       print(line);
+        for line in f:
+            data.append(line.strip());
+
+    for line in data:
+        print(line);
+
+    print("the noise that this makes is extremely annoying");
 
