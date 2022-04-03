d2916dfb57e11dfc2e43e5dc0fea8b62770e2058
root <root@BASTILLE.localdomain>
2022-04-03 15:19:10 -0400

    stuff

---
+++ william.java
@@ -0,0 +1,13 @@
+import java.util.*;
+import java.io.*;
+
+public class william {
+	public static void main(String[] args) throws Exception {
+		String bruh = "stuff";
+		System.out.println(bruh);
+		Scanner scanner = new Scanner(System.in);
+		String line = scanner.nextLine();
+		System.out.println("line: " + line);
+	}
+}
+
