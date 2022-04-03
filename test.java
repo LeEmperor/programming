428d1b8c14f27ff465f15e67be687c075e494a64
LeEmperor <bohdanpurtell@gmail.com>
2022-04-03 15:21:47 -0400

    stuff

---
+++ test.java
@@ -0,0 +1,9 @@
+import java.util.*;
+import java.io.*;
+
+public class test {
+    public static void main(String[] args) throws Exception {
+        String bruh = "stuff";
+        System.out.println(bruh);
+    }
+}
