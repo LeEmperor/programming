5aa012486b1f4f0d28dcbb5c5466cabb92916e30
LeEmperor <bohdanpurtell@gmail.com>
2022-04-12 13:38:57 -0400

    asdfas

--- aerospace.cpp
+++ aerospace.cpp
@@ -9,7 +9,7 @@ using namespace std;
 #define PB push_back
 #define exp 1e9
 #define all(x) (x).begin(), (x).end()
-#define nL "\n
+#define nL "\n"
 #define first a
 #define second b
 #define sz(x) (int((x).size())) 
@@ -26,20 +26,41 @@ typedef unsigned long int ui;
 typedef long long int ll;
 typedef unsigned long long int ull;
 
-void solve(string input) {
+class starship {
+	public:
+		string NAME; 
+		string CLASS;
+		int X;
+		int Y;
+	
+	public:
+		starship(string x, string y, int z, int w) {
+			NAME = x;
+			CLASS = y;
+			X = z;
+			Y = w;
+		}
+};
 
-
-
+string solve(VS input) {
+	return "nothing";	
 }
 
 int main() {
 	ios_base::sync_with_stdio(0);
 	cin.tie(0); cout.tie(0);
 	int testcases;
+	int dummy;
 	string line;
 	while(testcases--) {
-		cin >> line; 
-		cout << solve(line) << "\n";	
-	}	
+		cin >> dummy;
+		VS input;
+		while(dummy--) {
+			cin >> line;		
+			VS.PB(line);
+		}
+		cout << solve(VS) << "\n";
+
+	}
 }
 
