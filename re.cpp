388f7555cb4fb58611f0a421029f2a045d45e3a3
LeEmperor <bohdanpurtell@gmail.com>
2022-05-03 06:12:49 -0400

    asd

--- re.cpp
+++ re.cpp
@@ -31,10 +31,30 @@ struct ___ {
 };
 
 void solve(string s, string t) {
-	for(char c : t) {
-		if(count(s.begin(), s.end(), c) > 0) {
+	// case 1: characters in s are present in t		
+	for(char c : s) {
+		if(count(t.begin(), t.end(), c) > 0) {
+			// there is an instance of a letter l from s in t
 			cout << -1 << nL;
+			break;
 		}
+	}	
+
+	// case 2: both s and t are composed of the same thing
+	if(count(s.begin(), s.end(), t) == s.length()) {
+		cout << 1 << nL;
+	} else if(s.length() != 1) {
+		int bruh = 1;
+		int len = s.length();
+		for(int i = 1; i < len + 1; i++) {
+			bruh *= i;
+		}
+		cout << bruh << nL;
+	}
+
+	// case 3:
+	if(s.length() == 1) {
+		cout << 2 << nL;
 	}
 }
 
