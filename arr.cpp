b36f203530f43722bf6c80100d9a5be166b262be
LeEmperor <bohdanpurtell@gmail.com>
2022-06-09 21:42:45 -0400

    arr decremenets

--- arr.cpp
+++ arr.cpp
@@ -22,17 +22,64 @@ struct ___ {
 
 void solve(VI input1, VI input2) {
 	signed int diff;
+	int continueFrom = 0;
 	set<int> diffs;
+	
+	if(input1[0] >= input2[0] && input1.size() == 1) {
+		cout << "yes" << nL;
+		return;
+	} else if(input1[0] < input2[0] && input1.size() == 1) {
+		cout << "no" << nL;
+		return;
+	}
+	
+	bool bruh = true;
 	for(int i = 0; i < input1.size(); i++) {
-		diff = input1[i] - input2[i];
-		diffs.insert(diff);
+		if(input1[i] == input2[i]) {
+		} else {
+			bruh = false;
+			break;
+		} 
 	}
 
-	if(diffs.size() > 1) {
-		cout << "no" << nL;
-	} else {
-		cout << "yes\n";
+	if(bruh == true) {
+		cout << "yes" << nL;
+		return;
 	}
+	
+	for(int i = 0; i < input1.size(); i++) {
+		if(input1[i] < input2[i]) {
+			cout << "no" << nL;
+			return;
+		}
+	}
+	
+	// identify a base diff to compare
+	for(int i = 0; i < input1.size(); i++) {
+		if(input2[i] != 0) {
+			diff = input1[i] - input2[i];
+			continueFrom = i;
+			break;
+		}
+
+	}	
+
+	for(int i = continueFrom + 1; i < input1.size(); i++) {
+		int tempDiff = input1[i] - input2[i];
+		if(tempDiff != diff) {
+			// tempDiff is different from diff
+			if(input2[i] == 0 && input1[i] <= diff) {
+				// continue going
+			} else {
+				// array b[i] = 0, AND array a[i] > diff
+				cout << "no" << nL;
+				return;
+			}
+		} 
+	}
+
+	cout << "yes" << nL;
+	return;
 }
 
 int main() {
