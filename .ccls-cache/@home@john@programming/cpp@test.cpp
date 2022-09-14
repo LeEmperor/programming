a8add4461d2d3066c3f59a12c128c99e2e897d1f
LeEmperor <bohdanpurtell@gmail.com>
2022-09-14 02:24:50 -0400

    stud

--- .ccls-cache/@home@john@programming/cpp@test.cpp
+++ .ccls-cache/@home@john@programming/cpp@test.cpp
@@ -6,8 +6,6 @@ using namespace std;
 #define nL "\n"
 
 int main() {
-	cout << setw(33) << "Number of Novels Authored" << endl;
-	cout << setw(20) << left << "Author name" << 
-		setw(23) << right << "Number of novels" << endl;
+	cout << __cplusplus << nL;
 
 }
